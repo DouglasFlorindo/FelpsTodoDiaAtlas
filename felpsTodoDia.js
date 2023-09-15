f72d5f36336ed0b13e88cebeaf74131a014a3f1d
Douglas Florindo <douglasflorindodeassis@gmail.com>
2023-09-15 05:55:27 -0300

    Update

--- felpsTodoDia.js
+++ felpsTodoDia.js
@@ -12,15 +12,41 @@ async function populateAtlas() {
         novoImg.src = `../FelpsTodoDiaAtlas/Imagens/${felps.arquivo}.png`;
         novoImg.alt = felps.nome;
         novoImg.title = felps.nome;
-        novoImg.setAttribute("onClick", `mostrarModal(${felps.id})`)
-        novoImg.style.left = felps.posicao[0];
-        novoImg.style.top = felps.posicao[1];
+        novoImg.setAttribute("onClick", `mostrarModal(${felps.id})`);
+        novoImg.style.left = `${Math.floor(Math.random() * 91)}%`;
+        novoImg.style.top = `${Math.floor(Math.random() * 91)}%`;
 
         areaAtlas.appendChild(novoImg);
     }
-}
+};
+
+async function populateCatalogo() {
+
+    const response = await fetch("felps.json");
+    const felpsInfo = await response.json();
+
+    for (const felps of felpsInfo) {
+
+        const areaCatalogo = document.getElementById("areaCatalogo");
+        let novoDiv = document.createElement("div");
+        let novoImg = document.createElement("img");
+
+        novoDiv.className = "itemCatalogo";
+        novoImg.id = felps.id;
+        novoImg.className = "felpsCatalogo";
+        novoImg.src = `../FelpsTodoDiaAtlas/Imagens/${felps.arquivo}.png`;
+        novoImg.alt = felps.nome;
+        novoImg.title = felps.nome;
+        novoImg.setAttribute("onClick", `mostrarModal(${felps.id})`);
+
+        novoDiv.appendChild(novoImg);
+        areaCatalogo.appendChild(novoDiv);
+
+    }
+};
 
 populateAtlas();
+populateCatalogo();
 
 async function mostrarModal(felpsId) {
 
@@ -33,13 +59,15 @@ async function mostrarModal(felpsId) {
     document.getElementById("felpsHRes").title = felpsInfo[felpsId].nome;
     document.getElementById("subInfoModal").textContent = `${felpsInfo[felpsId].data} • ${felpsInfo[felpsId].artista}`
     document.getElementById("infoModal").showModal()
-}
+};
+
+document.querySelector("#botaoCatalogo").addEventListener("click", () => document.querySelector("#catalogoModal").showModal());
 
 function alterarEscala(x) {
 
     let root = document.documentElement;
     let tamanhoEscala = parseFloat(getComputedStyle(root).getPropertyValue("--escalaFelps"));
-    
+
     switch (x) {
         case true:
             root.style.setProperty("--escalaFelps", `${tamanhoEscala + 0.1}`)
@@ -49,7 +77,4 @@ function alterarEscala(x) {
             break
     }
 
-}
-
-
-
+};
