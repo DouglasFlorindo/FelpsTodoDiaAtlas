b7dd70840a366ba0931f84a061d9a6148f85c137
Douglas Florindo <douglasflorindodeassis@gmail.com>
2023-09-19 07:27:12 -0300

    Finalmente dá pra arrastar os felps!!!

--- felpsTodoDia.js
+++ felpsTodoDia.js
@@ -1,6 +1,8 @@
 const infoModal = document.querySelector("#infoModal");
 const catalogoModal = document.querySelector("#catalogoModal");
-let felpsAtualId = 0;
+let felpsAtualNum;
+let felpsDrag;
+let movendo = false;
 
 async function populateAtlas() {
 
@@ -9,18 +11,25 @@ async function populateAtlas() {
 
     for (const felps of felpsInfo) {
 
-        const areaAtlas = document.getElementById("areaAtlas");
         let novoImg = document.createElement("img");
-        novoImg.id = felps.id;
+        novoImg.id = `f${felps.id}`;
         novoImg.classList = "felps clicavel";
         novoImg.src = `../FelpsTodoDiaAtlas/Imagens/${felps.arquivo}.png`;
         novoImg.alt = felps.nome;
         novoImg.title = felps.nome;
-        novoImg.setAttribute("onClick", `mostrarModal(${felps.id})`);
-        novoImg.style.left = `${Math.floor(Math.random() * (96 - -5) + -5)}%`;
-        novoImg.style.top = `${Math.floor(Math.random() * 81)}%`;
+        novoImg.addEventListener('click', () => mostrarModal(felps.id));
+        novoImg.addEventListener('mousedown', function () {
+            felpsDrag = this;
+        });
+        novoImg.addEventListener('mouseup', function () {
+            setTimeout(() => {
+                movendo = false
+            }, 500);
+        })
+        novoImg.style.left = `${Math.floor(Math.random() * (90 - 20 + 1)) + 10}%`;
+        novoImg.style.top = `${Math.floor(Math.random() * (70 - 20 + 1)) + 20}%`;
 
-        areaAtlas.appendChild(novoImg);
+        document.body.appendChild(novoImg);
     }
 };
 
@@ -50,20 +59,21 @@ async function populateCatalogo() {
 };
 
 async function mostrarModal(felpsId) {
+    if (movendo == false) {
+        const response = await fetch("felps.json");
+        const felpsInfo = await response.json();
+        felpsAtualId = felpsId
 
-    const response = await fetch("felps.json");
-    const felpsInfo = await response.json();
-    felpsAtualId = felpsId
+        document.querySelector("#tituloModal").textContent = `${felpsInfo[felpsId].numero}. ${felpsInfo[felpsId].nome}`;
+        document.querySelector("#felpsHRes").src = `../FelpsTodoDiaAtlas/Imagens/${felpsInfo[felpsId].arquivo}HRes.png`;
+        document.querySelector("#felpsHRes").alt = felpsInfo[felpsId].nome;
+        document.querySelector("#felpsHRes").title = felpsInfo[felpsId].nome;
+        document.querySelector("#subInfoModal").textContent = `${felpsInfo[felpsId].data} • ${felpsInfo[felpsId].artista}`;
+        document.querySelector("#linkTwitter").setAttribute("href", `${felpsInfo[felpsId].tweet}`);
+        document.querySelector("#linkOriginal").setAttribute("href", `${felpsInfo[felpsId].original}`);
 
-    document.querySelector("#tituloModal").textContent = `${felpsInfo[felpsId].numero}. ${felpsInfo[felpsId].nome}`;
-    document.querySelector("#felpsHRes").src = `../FelpsTodoDiaAtlas/Imagens/${felpsInfo[felpsId].arquivo}HRes.png`;
-    document.querySelector("#felpsHRes").alt = felpsInfo[felpsId].nome;
-    document.querySelector("#felpsHRes").title = felpsInfo[felpsId].nome;
-    document.querySelector("#subInfoModal").textContent = `${felpsInfo[felpsId].data} • ${felpsInfo[felpsId].artista}`;
-    document.querySelector("#linkTwitter").setAttribute("href", `${felpsInfo[felpsId].tweet}`);
-    document.querySelector("#linkOriginal").setAttribute("href", `${felpsInfo[felpsId].original}`);
-
-    infoModal.showModal()
+        infoModal.showModal()
+    }
 };
 
 populateAtlas();
@@ -94,7 +104,24 @@ function alterarEscala(x) {
     }
 };
 
-let inputSenha = ""
+document.addEventListener('mousedown', function (e) {
+    if (e.target.tagName === 'IMG') {
+        e.preventDefault();
+    }
+});
+document.addEventListener('mouseup', () => felpsDrag = null);
+document.addEventListener('mousemove', function (e) {
+    if (felpsDrag != null) {
+        let x = e.pageX;
+        let y = e.pageY;
+
+        felpsDrag.style.left = x + "px";
+        felpsDrag.style.top = y + "px";
+        movendo = true
+    }
+})
+
+let inputSenha;
 
 document.addEventListener("keydown", function (event) {
     if (event.key == "'" || event.key == "-" && inputSenha.length < 4) {
@@ -118,4 +145,6 @@ function verificarSenha(senha) {
 function easterEgg() {
     console.log("easterEgg ativado!!!")
 }
+
+// ok, cada felps precisa de dois event listeners, pra mouseup e down, que irão ativar funções utilizando o id como argumento.
 
