310c87285cb8928cd6b7757fb44acdb1f9b3d9fa
Douglas Florindo <douglasflorindodeassis@gmail.com>
2024-06-05 15:24:29 -0300

    update bobo

--- Scripts/felpsTodoDia.js
+++ Scripts/felpsTodoDia.js
@@ -114,7 +114,7 @@ document.addEventListener("keydown", function (event) {
 });
 
 window.addEventListener("load", () => {
-    print("carregado")
+    console.log("Recursos carregados")
     tituloPrincipal.textContent = "Felps Todo Dia Atlas";
     tituloPrincipal.classList = "";
 })
